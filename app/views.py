e36ca9efe8950f09d35eab4c123e67e775b64415
FloWambui <gflorencewambui@gmail.com>
2022-06-12 23:14:23 +0300

    add the app configurations

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
-# Create your views here.
+
+app_name= 'app'
+
+def home(request):
+    return HttpResponse('Welcome to the world of Awards - Vote your project!')
