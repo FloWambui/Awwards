20bc31bb3c527d5aab75f8d884cfa103a6c1ee17
FloWambui <gflorencewambui@gmail.com>
2022-06-13 23:34:06 +0300

    add testcase for rates models

--- app/tests.py
+++ app/tests.py
@@ -19,3 +19,17 @@ class ProfileTestCase(TestCase):
         self.profile.save_profile()
         profile = Profile.objects.all()
         self.assertTrue(len(profile) > 0)
+
+
+class RatesTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='Suni')
+        self.user.save()
+        self.new_profile = Profile(id = 1,profile_pic='profile.png',bio='this is a test profile',user=self.user)
+        self.new_profile.save()
+        self.new_project = Project(image='profile.png',title="image",url='http', description='test profile description', date='13/06/2022',
+        profile=self.new_profile)
+        self.rate = Rates(design='10',usability='8',content='6',project = self.new_project, date="13/06/2022")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rate, Rates))
